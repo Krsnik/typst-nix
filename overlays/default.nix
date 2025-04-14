72b364f056c246994e9e9d083301d331cc8d8828
Kristian Krsnik <git@krsnik.at>
2025-04-14 10:50:01 +0200

    fixed overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,8 +1,16 @@
-{self, ...}: rec {
-  default = typstPackages;
+{self, ...}: let
+  overlays = {
+    typst = final: prev: {
+      typst = self.inputs.nixpkgs.legacyPackages.${prev.system}.typst;
+    };
 
-  typstPackages = final: prev: {
-    typst = self.pkgs.${prev.system}.typst;
-    typstPackages = self.typstPackages.${prev.system};
+    typstPackages = final: prev: {
+      typstPackages = self.typstPackages.${prev.system};
+    };
   };
-}
+in
+  overlays
+  // {
+    # Expose all package overlays as one overlay
+    default = self.inputs.nixpkgs.lib.composeManyExtensions (builtins.attrValues overlays);
+  }
