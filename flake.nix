4fe04ee371440384d7fabee3a7d7271074c00b8f
Kristian Krsnik <git@krsnik.at>
2025-04-27 19:59:00 +0200

    fixed bug

--- flake.nix
+++ flake.nix
@@ -37,28 +37,27 @@
     in
       attrsets.filterAttrs (name: value: isDerivation value) self.typstPackages.${system});
 
-    hello = "hello";
-    # src: entrypoint: let
-    #   removePrefix = prefix: source: let
-    #     sourceLen = builtins.stringLength source;
-    #     prefixLen = builtins.stringLength prefix;
-    #     _checkPrefixIsContained =
-    #       if (builtins.substring 0 prefixLen source) != prefix
-    #       then throw "Source string does not contain prefix '${prefix}'."
-    #       else true;
-    #   in
-    #     assert _checkPrefixIsContained;
-    #       builtins.substring prefixLen (sourceLen - prefixLen) source;
-    #   stripStorePrefix = storePath: let
-    #     rootPath = ./.;
-    #     rootPathString = builtins.toString rootPath;
-    #     storePathString = builtins.toString storePath;
-    #   in
-    #     ./. + "${removePrefix rootPathString storePathString}";
-    # in {
-    #   path = stripStorePrefix src;
-    #   inherit entrypoint;
-    # };
+    out = src: entrypoint: let
+      removePrefix = prefix: source: let
+        sourceLen = builtins.stringLength source;
+        prefixLen = builtins.stringLength prefix;
+        _checkPrefixIsContained =
+          if (builtins.substring 0 prefixLen source) != prefix
+          then throw "Source string does not contain prefix '${prefix}'."
+          else true;
+      in
+        assert _checkPrefixIsContained;
+          builtins.substring prefixLen (sourceLen - prefixLen) source;
+      stripStorePrefix = storePath: let
+        rootPath = ./.;
+        rootPathString = builtins.toString rootPath;
+        storePathString = builtins.toString storePath;
+      in
+        ./. + "${removePrefix rootPathString storePathString}";
+    in {
+      path = stripStorePrefix src;
+      inherit entrypoint;
+    };
 
     mkLib = args @ {
       pkgs,
